80cfe1fac25aeb597ce23ff7e4e63cf613c4acbb
Robert Hilton <robert.a.hilton.jr@gmail.com>
2021-12-19 16:48:57 +0000

    Remove GL2

--- src/WindowManager.cpp
+++ src/WindowManager.cpp
@@ -6,7 +6,6 @@
 
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_sdl.h"
-#include "imgui/imgui_impl_opengl2.h"
 #include "imgui/imgui_impl_opengl3.h"
 #include <stdio.h>
 #include <SDL2/SDL.h>
@@ -51,100 +50,6 @@ void WindowManager::resetUpdateRequested()
     std::scoped_lock<std::mutex> lock(mUpdateMutex);
     mUpdateRequested = false;
 }
-
-// [[deprecated("Replaced by OpenGL3 Method")]]
-// void WindowManager::showWindowOpenGL2_internal()
-// {
-//     // Setup SDL
-//     // (Some versions of SDL before <2.0.10 appears to have performance/stalling issues on a minority of Windows systems,
-//     // depending on whether SDL_INIT_GAMECONTROLLER is enabled or disabled.. updating to latest version of SDL is recommended!)
-//     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
-//     {
-//         printf("Error: %s\n", SDL_GetError());
-//         return;
-//     }
-
-//     // Setup window
-//     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-//     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-//     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-//     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-//     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-//     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-//     SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
-//     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-//     SDL_GL_MakeCurrent(window, gl_context);
-//     SDL_GL_SetSwapInterval(1); // Enable vsync
-
-//     // Setup Dear ImGui context
-//     IMGUI_CHECKVERSION();
-//     ImGui::CreateContext();
-//     ImGuiIO& io = ImGui::GetIO(); (void)io;
-//     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-//     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-
-//     // Setup Dear ImGui style
-//     ImGui::StyleColorsDark();
-//     //ImGui::StyleColorsClassic();
-
-//     // Setup Platform/Renderer backends
-//     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-//     ImGui_ImplOpenGL2_Init();
-
-//     // Our state
-//     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-//     // Main loop
-//     bool done = false;
-//     while (!done)
-//     {
-//         // Poll and handle events (inputs, window resize, etc.)
-//         // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-//         // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-//         // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-//         // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-//         SDL_Event event;
-//         while (SDL_PollEvent(&event))
-//         {
-//             ImGui_ImplSDL2_ProcessEvent(&event);
-//             if (event.type == SDL_QUIT)
-//                 done = true;
-//             if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
-//                 done = true;
-//         }
-
-//         // Start the Dear ImGui frame
-//         ImGui_ImplOpenGL2_NewFrame();
-//         ImGui_ImplSDL2_NewFrame();
-//         ImGui::NewFrame();
-
-
-//         drawImguiWindow(1);
-
-
-//         // Rendering
-//         ImGui::Render();
-//         glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-//         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
-//         glClear(GL_COLOR_BUFFER_BIT);
-//         //glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound
-//         ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-//         SDL_GL_SwapWindow(window);
-//     }
-
-//     // Cleanup
-//     ImGui_ImplOpenGL2_Shutdown();
-//     ImGui_ImplSDL2_Shutdown();
-//     ImGui::DestroyContext();
-
-//     SDL_GL_DeleteContext(gl_context);
-//     SDL_DestroyWindow(window);
-//     SDL_Quit();
-
-//     ros::shutdown();
-
-//     return;
-// }
 
 void WindowManager::showWindowOpenGL3_internal()
 {
